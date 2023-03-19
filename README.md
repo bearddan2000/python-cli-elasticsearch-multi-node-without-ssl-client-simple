d4db3298af6b047fc8c0149cb2a28759857674f6
bearddan2000 <bearddan2000@yahoo.com>
2023-03-19 00:01:57 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# python-cli-elasticsearch-multi-node-without-ssl-client-simple
+
+## Description
+Reads a multi node cluster for data in `dog-demo` document.
+
+## Tech stack
+- python
+- elasticsearch
+- kibana
+
+## Docker stack
+- python
+- elasticsearch
+- kibana
+
+## To run
+`sudo ./install.sh -u`
+
+## To stop (optional)
+`sudo ./install.sh -d`
+
+## For help
+`sudo ./install.sh -h`
+
+## Credit
+- [Docker setup](https://github.com/jeroenhendricksen/elasticsearch-docker-cluster)
+- [Search setup](https://www.elastic.co/guide/en/elasticsearch/client/python-api/master/examples.html)
---
+++ py-srv/bin/data.py
@@ -0,0 +1,26 @@
+from settings import COLUMN_NAME
+
+INDEX_NAME = "dog-demo"
+
+DOC = [
+    {
+        COLUMN_NAME['col_0']['name']: 1, 
+        COLUMN_NAME['col_1']['name']: 'Doberman', 
+        COLUMN_NAME['col_2']['name']: 'brown'
+    },
+    {
+        COLUMN_NAME['col_0']['name']: 2, 
+        COLUMN_NAME['col_1']['name']: 'Dashound', 
+        COLUMN_NAME['col_2']['name']: 'grey'
+    },
+    {
+        COLUMN_NAME['col_0']['name']: 3, 
+        COLUMN_NAME['col_1']['name']: 'Bulldog', 
+        COLUMN_NAME['col_2']['name']: 'white'
+    },
+    {
+        COLUMN_NAME['col_0']['name']: 4, 
+        COLUMN_NAME['col_1']['name']: 'Gr Shepard', 
+        COLUMN_NAME['col_2']['name']: 'brown'
+    }
+]
